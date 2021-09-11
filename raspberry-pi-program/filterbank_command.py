13cfeadb22cbf8c7ff50dbb202ef2886d2ad9c9c
Reese <rgrimsle@andrew.cmu.edu>
2021-09-11 14:54:51 -0400

    GPIO set mode

--- raspberry-pi-program/filterbank_command.py
+++ raspberry-pi-program/filterbank_command.py
@@ -15,11 +15,22 @@ def i2c_setup(bus_id=I2C_BUS_ID):
     return bus
 
 def i2c_read_cap(bus, address=CAP_I2C_ADDR):
-
-    read_bytes = bus.read_byte_data(address, 0) #this doesn't actually need an address..
+    reg_addr = 0 #datashet says we don't need a register address..
+    read_bytes = bus.read_byte_data(address, reg_addr)
     return read_bytes
 
+def i2c_write_cap_value_nonvolatile(bus, value, address=CAP_I2C_ADDR):
+    assert value >=0 and value < 0x200 #9 bit value, unsigned
+    byte_1 = 0 | ((value & 0x100) >> 8) # first config byte. LSbit is most signicant of 9-bit value
+    byte_2 = value & 0xFF #second config byte; lower 8 bits of cap value
+    bus.write_byte_data(address, byte_1, byte_2)
+
+def i2c_write_cap_value_volatile(*args, **kwargs):
+    raise NotImplementedError()
+
+
 def setup_gpio_pins(m0_pin=MUX_0_PIN, m1_pin=MUX_1_PIN):
+    GPIO.setwarnings(False) # this is probably a bad idea...
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(m0_pin, GPIO.OUT, initial=GPIO.HIGH)
     GPIO.setup(m1_pin, GPIO.OUT, initial=GPIO.HIGH)
@@ -38,7 +49,7 @@ def enable_matching_circuit(circuit_num, m0_pin=MUX_0_PIN, m1_pin=MUX_1_PIN):
 
 if __name__ == "__main__":
     setup_gpio_pins()
-    i2c_setup()
+    bus = i2c_setup()
 
     #do a startup sequence to show all the options
     enable_matching_circuit(3)
@@ -51,14 +62,18 @@ if __name__ == "__main__":
 
     while True:
         print('CLI:')
-        print('  - z (integer) will activate matching circuit')
-        print('  - c (integer) will set a value for the digitally tuned capacitor (value space TBD)')
+        print('  - "z (integer)" will activate matching circuit')
+        print('  - "c (integer)" will set a value for the digitally tuned capacitor (value space TBD)')
+        print('  - "r" will display information about the current configuation')
         input_str = input('Input matching circuit to configure (an integer)\n')
         print()
         try:
             if input_str[0] == 'z':
-                matching_circuit_num = int(input_str)
+                matching_circuit_num = int(input_str[1:])
                 enable_matching_circuit(matching_circuit_num)
-            elif
-        except ValueError:
-            print('Invalid matching circuit; try an integer between 0 and 3')
+            elif input_str[0] =='c':
+                cap_value = int(input_str[1:])
+                i2c_write_cap_value_volatile(bus, cap_value)
+        except ValueError as e:
+            print('Invalid value; try an integer between 0 and 3')
+            print(e)
